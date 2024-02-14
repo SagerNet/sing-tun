0c708a2958d4fcf954a1f434d3291aa421532742
世界 <i@sekai.icu>
2024-02-14 12:51:48 +0800

    Update gVisor to 20240206.0

--- stack_mixed.go
+++ stack_mixed.go
@@ -56,7 +56,7 @@ func (m *Mixed) Start() error {
 			if err != nil {
 				return
 			}
-			udpConn := gonet.NewUDPConn(ipStack, &wq, endpoint)
+			udpConn := gonet.NewUDPConn(&wq, endpoint)
 			lAddr := udpConn.RemoteAddr()
 			rAddr := udpConn.LocalAddr()
 			if lAddr == nil || rAddr == nil {
