06b4d4ecd19de37ff446d794f98ede38c803c0a6
世界 <i@sekai.icu>
2024-11-22 15:45:38 +0800

    Add tailscale checksum

---
+++ internal/checksum_test/sum_bench_test.go
@@ -0,0 +1,33 @@
+package checksum_test
+
+import (
+	"crypto/rand"
+	"testing"
+
+	"github.com/sagernet/sing-tun/internal/gtcpip/checksum"
+	"github.com/sagernet/sing-tun/internal/tschecksum"
+)
+
+func BenchmarkTsChecksum(b *testing.B) {
+	packet := make([][]byte, 1000)
+	for i := 0; i < 1000; i++ {
+		packet[i] = make([]byte, 1500)
+		rand.Read(packet[i])
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		tschecksum.Checksum(packet[i%1000], 0)
+	}
+}
+
+func BenchmarkGChecksum(b *testing.B) {
+	packet := make([][]byte, 1000)
+	for i := 0; i < 1000; i++ {
+		packet[i] = make([]byte, 1500)
+		rand.Read(packet[i])
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		checksum.Checksum(packet[i%1000], 0)
+	}
+}
--- internal/gtcpip/checksum/checksum_unsafe.go
+++ internal/gtcpip/checksum/checksum_unsafe.go
@@ -1,3 +1,5 @@
+//go:build !amd64
+
 // Copyright 2023 The gVisor Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
