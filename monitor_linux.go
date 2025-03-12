9105485a50c23e2fd587bf6480bc094d72c2d9a6
greathongtu <69706194+greathongtu@users.noreply.github.com>
2025-03-12 16:36:05 +0800

    Fix typo of 'Android' in ErrNetlinkBanned error message

--- monitor_linux.go
+++ monitor_linux.go
@@ -27,7 +27,7 @@ type networkUpdateMonitor struct {
 var ErrNetlinkBanned = E.New(
 	"netlink socket in Android is banned by Google, " +
 		"use the root or system (ADB) user to run sing-box, " +
-		"or switch to the sing-box Adnroid graphical interface client",
+		"or switch to the sing-box Android graphical interface client",
 )
 
 func NewNetworkUpdateMonitor(logger logger.Logger) (NetworkUpdateMonitor, error) {
