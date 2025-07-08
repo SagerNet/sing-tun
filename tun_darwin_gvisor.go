4c81c8a62a832bdc11a73eb57bf8d1de41c9d68a
世界 <i@sekai.icu>
2025-07-08 15:44:35 +0800

    Fix usages of readmsg_x

--- tun_darwin_gvisor.go
+++ tun_darwin_gvisor.go
@@ -12,14 +12,17 @@ var _ GVisorTun = (*NativeTun)(nil)
 
 func (t *NativeTun) NewEndpoint() (stack.LinkEndpoint, stack.NICOptions, error) {
 	ep, err := fdbased.New(&fdbased.Options{
-		FDs:               []int{t.tunFd},
-		MTU:               t.options.MTU,
-		RXChecksumOffload: true,
+		FDs:                []int{t.tunFd},
+		MTU:                t.options.MTU,
+		RXChecksumOffload:  true,
+		PacketDispatchMode: fdbased.RecvMMsg,
 	})
 	if err != nil {
 		return nil, stack.NICOptions{}, err
 	}
-	return ep, stack.NICOptions{
-		QDisc: fifo.New(ep, 1, 1000),
-	}, nil
+	var nicOptions stack.NICOptions
+	if t.options.MTU < 49152 {
+		nicOptions.QDisc = fifo.New(ep, 1, 1000)
+	}
+	return ep, nicOptions, nil
 }
